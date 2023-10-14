68bc56985b07da859e5cd422ad08c74770d55bea
Qunnnn <TranVanQuan20211@gmail.com>
2023-10-14 22:19:47 +0700

    function and arrow-function

--- syntax/syntax.js
+++ syntax/syntax.js
@@ -2,15 +2,27 @@ const fullName = 'Văn Quân'; //inchangeable
 let age = 22;  //changeable 
 const hasHobbies = true; //inchangeable
 
-function summarizeUser(userName, userAge, userHasHobby) {
-    return (
-        'My name is ' +
-        userName +
-        ', age is ' +
-        userAge +
-        ' and the user has hobbies: ' +
-        userHasHobby
-    );
-}
 
+
+// const summarizeUser =
+//     function (userName, userAge, userHasHobby) {
+//         return (
+//             'My name is ' +
+//             userName +
+//             ', age is ' +
+//             userAge +
+//             ' and the user has hobbies: ' +
+//             userHasHobby
+//         );
+//     }
+
+// arrow_function 
+const summarizeUser = (userName, userAge, userHasHobby) => 'My name is ' + userName + ', age is ' + userAge + ', and the user has hobbies: ' + userHasHobby;
 console.log(summarizeUser(fullName, age, hasHobbies));
+
+const addNum = (a ,b) => a + b; 
+console.log(addNum(1,2));
+
+const addOne = a => ++a;
+console.log(addOne(2));
+
