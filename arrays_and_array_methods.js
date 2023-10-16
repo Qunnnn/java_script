14634187526dc7f7cc165c25cdfca52972d5f535
Qunnnn <TranVanQuan20211@gmail.com>
2023-10-16 22:10:10 +0700

    arrays and array methods in JS

---
+++ arrays_and_array_methods.js
@@ -0,0 +1,8 @@
+// const array =  ['Sports' , 'Cooking' , 'Esports' , 1 , {} , true];  can contain multiple types
+
+const array = ['Sports' , 'Cooking' , 'Esports'];
+
+// array methods 
+
+console.log(array.map((hobby) => 'Hobby: ' +hobby))
+console.log(array);
