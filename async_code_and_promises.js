8808af7cf1265ef181d2b195e8e02f32d81e1356
Qunnnn <TranVanQuan20211@gmail.com>
2023-10-18 23:36:41 +0700

    async code and promises in JS

---
+++ async_code_and_promises.js
@@ -0,0 +1,23 @@
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(
+            () => resolve('Done!'),
+            1500,
+        );
+    });
+    return promise;
+}
+
+setTimeout(() => {
+    console.log('Timer is done');
+    fetchData().then(test => {
+        console.log(test);
+    });
+}, 2000
+);
+
+console.log('Hello');
+console.log('Hi');
+
+
+
